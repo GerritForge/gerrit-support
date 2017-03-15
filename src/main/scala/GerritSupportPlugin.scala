90c23d7e94990b5a5a8eb91e730ea62e1d0f8664
Claudio Pacchiega <claudio.pacchiega@gmail.com>
2017-03-15 16:57:32 +0100

    Scalatra support
    Change-Id: I5588fed70626b46541e9ebb64d11ae2f821e5f7e

--- src/main/scala/GerritSupportPlugin.scala
+++ src/main/scala/GerritSupportPlugin.scala
@@ -1,17 +1,28 @@
+package com.googlesource.gerrit.plugins.validators
+
 import com.google.inject._
 import com.google.gerrit.extensions.annotations._
-import javax.servlet.http._
+
+import org.scalatra.ScalatraServlet
+
 
 @Singleton
-@Export("/")
-class GerritSupportPlugin extends HttpServlet {
+@Export("/*")
+class GerritSupportPlugin extends ScalatraServlet {
 
-  override def doGet(req: HttpServletRequest, resp: HttpServletResponse) = {
-
-    resp.getWriter print
-      "[GerritSupportPlugin initial template] "+Scala.list.mkString("-");
+  get("/") {
+    "[GerritSupportPlugin working with scalatra ] \n"+Scala.list.mkString("-") + "\n" ;
   }
+
+  post("/post") {
+    "[GerritSupportPlugin called with post] :'" + request.body + "'\n"
+  }
+
 }
+
+
+
+
 // this has been inserted to be sure we can access scala runtime from Gerrit
 object Scala {
   val list = List("1","2","3");
